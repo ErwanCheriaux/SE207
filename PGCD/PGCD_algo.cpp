30d0903cc33d72ef1179df6079e14dc80e01eaf9
Erwan CHERIAUX <erwan.cheriaux@telecom-paristech.fr>
2017-06-02 14:51:21 +0200

    algo du PGCD

---
+++ PGCD/PGCD_algo.cpp
@@ -0,0 +1,38 @@
+//PGCD_algo.cpp
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <cstdint>
+#include <algorithm>
+
+using namespace std;
+
+unsigned int pgcd(unsigned int a, unsigned int b)
+{
+   unsigned int MAX = max(a,b);
+   unsigned int MIN = min(a,b);
+
+   while(MAX > 0 and MIN >0)
+   {
+      MAX = MAX - MIN;
+
+      unsigned int tmp = max(MAX,MIN);
+
+      MIN = min(MAX,MIN);
+      MAX = tmp;
+   }
+
+   return MAX;
+}
+
+int main(void)
+{
+   cout << "pgcd entre 5 et 10    : " << pgcd(5, 10) << endl;
+   cout << "pgcd entre 9 et 10    : " << pgcd(9, 10) << endl;
+   cout << "pgcd entre 2 et 10    : " << pgcd(2, 10) << endl;
+   cout << "pgcd entre 456 et 842 : " << pgcd(456, 842) << endl;
+   cout << "pgcd entre 100 et 150 : " << pgcd(100, 150) << endl;
+   cout << "pgcd entre 222 et 111 : " << pgcd(222, 111) << endl;
+   return 0;
+}
