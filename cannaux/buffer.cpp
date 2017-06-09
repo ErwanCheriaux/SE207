95cf8b7df3325a613537717a0f65f13d80d246a5
Erwan CHERIAUX <erwan.cheriaux@telecom-paristech.fr>
2017-06-09 16:48:15 +0200

    buffer console

--- cannaux/buffer.cpp
+++ cannaux/buffer.cpp
@@ -7,9 +7,8 @@ SC_MODULE(module)
 {
    sc_in <bool> buffer;
    sc_in <bool> signal;
-   sc_out<bool> out;
 
-   SC_CTOR(module):buffer("buffer"), signal("signal"), out("out")
+   SC_CTOR(module):buffer("buffer"), signal("signal")
    {
       SC_METHOD(method);
       sensitive << buffer << signal;
@@ -18,7 +17,7 @@ SC_MODULE(module)
 
    void method()
    {
-      out = !out;
+      cout << "...NOTIFICATION !" << endl;
    }
 };
 
@@ -27,12 +26,10 @@ int sc_main (int argc, char * argv[])
 {
    sc_buffer<bool> buffer;
    sc_signal<bool> signal;
-   sc_signal<bool> out;
    module myModule("myModule");
 
    myModule.buffer(buffer);
    myModule.signal(signal);
-   myModule.out(out);
 
    // trace VCD
    sc_trace_file *trace_f;
@@ -40,28 +37,25 @@ int sc_main (int argc, char * argv[])
    trace_f->set_time_unit(1,SC_NS);
    sc_trace(trace_f, buffer, "buffer");
    sc_trace(trace_f, signal, "signal");
-   sc_trace(trace_f, out,    "out");
 
    buffer = false;
    signal = false;
 
    sc_start(10, SC_NS);
 
+   cout << "Buffer = Buffer..." << endl;
    buffer = buffer;
    sc_start(1, SC_NS);
-   buffer = buffer;
-   sc_start(1, SC_NS);
-   buffer = !buffer;
-   sc_start(1, SC_NS);
+
+   cout << "Buffer = NOT(Buffer)..." << endl;
    buffer = !buffer;
    sc_start(1, SC_NS);
 
+   cout << "Signal = Signal..." << endl;
    signal = signal;
    sc_start(1, SC_NS);
-   signal = signal;
-   sc_start(1, SC_NS);
-   signal = !signal;
-   sc_start(1, SC_NS);
+
+   cout << "Signal = NOT(Signal)..." << endl;
    signal = !signal;
    sc_start(1, SC_NS);
 
