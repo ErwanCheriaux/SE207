45653f7a9b198b34095621d819249382a2f8f6f3
Erwan CHERIAUX <erwan.cheriaux@gmail.com>
2017-06-15 18:23:40 +0200

    trace VCD

--- cannaux/fifo.cpp
+++ cannaux/fifo.cpp
@@ -44,11 +44,23 @@ SC_MODULE(consomateur)
 
 int sc_main (int argc, char * argv[])
 {
+   sc_signal<bool> clk;
    sc_fifo<int> fifo(50);
 
    producteur  P("producteur");
    consomateur C("consomateur");
 
    P.in(fifo);
+   P.clk(clk);
    C.out(fifo);
+   C.clk(clk);
+
+   // trace VCD
+   sc_trace_file *trace_f;
+   trace_f = sc_create_vcd_trace_file ("buffer");
+   trace_f->set_time_unit(1,SC_NS);
+   sc_trace(trace_f, clk, "clk");
+   sc_trace(trace_f, fifo, "fifo");
+
+   return 0;
 }
