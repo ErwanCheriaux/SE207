e56fc5ee37cfe36b0eb9b93b47a15dc796100e1b
Erwan CHERIAUX <erwan.cheriaux@telecom-paristech.fr>
2017-06-16 14:35:23 +0200

    video_out.h

---
+++ projet_video/video_out.h
@@ -0,0 +1,57 @@
+#ifndef VIDEO_OUT_H
+#define VIDEO_OUT_H
+
+#include <systemc.h>
+#include "image.h"
+
+/***************************************
+ *  définition du module
+ **************************************/
+SC_MODULE(VIDEO_OUT) {
+
+   // IO PORTS
+   sc_in<bool>  clk;
+   sc_in<bool>  reset_n;
+
+   sc_in<bool> href;
+   sc_in<bool> vref;
+
+   sc_in<unsigned char> pixel_in;
+
+   /***************************************************
+    *  constructeur
+    **************************************************/
+   SC_CTOR(VIDEO_OUT):base_name("wallace")
+   {
+      cout << "Instanciation de " << name() <<" ..." ;
+
+      SC_THREAD(write_image);
+      sensitive << clk.pos();
+      async_reset_signal_is(reset_n,false);
+      dont_initialize();
+
+      current_image_number = 0;
+      reset_done  = false;
+      image.pixel = NULL;
+      read_port();
+
+      cout << "... réussie" << endl;
+   }
+
+   /***************************************************
+    *  méthodes et champs internes
+    **************************************************/
+   private:
+
+   void read_port();
+   void write_image();
+
+   const std::string base_name;              // nom de base des images d'entrée
+   int               current_image_number;   // numéro de l'image courante
+   bool              reset_done;
+
+   Image             image;
+};
+
+#endif
+
